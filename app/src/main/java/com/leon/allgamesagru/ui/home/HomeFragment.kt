7eec020a3bd15b63a954b2949addfe8620c8c49d
Leon4ikL <uraltsev_leon@icloud.com>
2022-04-20 13:24:43 +0400

    Создание home fragment и его макета.

---
+++ app/src/main/java/com/leon/allgamesagru/ui/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.leon.allgamesagru.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leon.allgamesagru.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
