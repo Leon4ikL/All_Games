02e0d85eeeb4c4ecfb2ac74ab2301a0c274ad0ef
Leon4ikL <uraltsev_leon@icloud.com>
2022-04-21 19:26:57 +0400

    Реализация вертикального списка. Реализация Горизонтального списка в вертикальных элементах

--- app/src/main/java/com/leon/allgamesagru/ui/home/HomeFragment.kt
+++ app/src/main/java/com/leon/allgamesagru/ui/home/HomeFragment.kt
@@ -6,7 +6,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.leon.allgamesagru.R
+import com.leon.allgamesagru.databinding.FragmentHomeBinding
+import com.leon.allgamesagru.ui.adapter.HorizontalGameAdapter
+import com.leon.allgamesagru.ui.adapter.VerticalHomeScreenAdapter
+import com.leon.allgamesagru.ui.models.CategoryGameList
+import com.leon.allgamesagru.ui.models.GameItem
+import java.lang.RuntimeException
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding ?: throw RuntimeException("Home Fragment Binding == null")
+
+
+    private val verticalHomeScreenAdapter = VerticalHomeScreenAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialization()
+    }
+
+    private fun initialization() {
+        with(binding){
+            verticalHomeScreenAdapter.list = listOf(
+                CategoryGameList(0,"Popularity", listOf(
+                    GameItem(0,1,"Game 1"),
+                    GameItem(0,1,"Game 2"),
+                    GameItem(0,1,"Game 3")
+                ))
+            )
+            rvVertical.adapter = verticalHomeScreenAdapter
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
---
+++ app/src/main/java/com/leon/allgamesagru/ui/models/CategoryGameList.kt
@@ -0,0 +1,7 @@
+package com.leon.allgamesagru.ui.models
+
+data class CategoryGameList(
+    val id: Int,
+    val title: String,
+    val games: List<GameItem>
+)
